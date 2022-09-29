728e0eb111fe6e931898afb9dba124b7ac39fe79
gentil001 <113639960+gentil001@users.noreply.github.com>
2022-09-29 18:17:47 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String a= first_name;
+    String B= last_name;
+    int c=2;
+    int d=8;
+    String.valueOf(c).getBytes();
+    }
+}
